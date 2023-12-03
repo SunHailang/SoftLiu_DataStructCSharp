82b577d4be2eecbb5b79893bc46815abbfd45612
SunHailang <shl971277663@outlook.com>
2023-12-03 20:43:17 +0800

    add leetcode 20231203

--- SoftLiu_DataStructCSharp/LeetCode/LeetCodeApp.cs
+++ SoftLiu_DataStructCSharp/LeetCode/LeetCodeApp.cs
@@ -13,7 +13,92 @@ namespace SoftLiu_DataStructCSharp.LeetCode
             //Time20231128.Run();
             //Time20231129.Run();
             //Time20231130.Run();
-            Time20231202.Run();
+            //Time20231202.Run();
+            //Time20231203.Run();
+
+
+            LeetCodeRun();
         }
+
+        public static void LeetCodeRun()
+        {
+            int[] nums = new int[] { 3, 2, 1, 0, 4 };
+            Console.WriteLine(CanJump(nums));
+        }
+
+        #region 45. 跳跃游戏 II
+        /// <summary>
+        /// 给定一个长度为 n 的 0 索引整数数组 nums。初始位置为 nums[0]。
+        /// 每个元素 nums[i] 表示从索引 i 向前跳转的最大长度。
+        /// 换句话说，如果你在 nums[i] 处，你可以跳转到任意 nums[i + j] 处:
+        /// 0 <= j <= nums[i]
+        /// i + j < n
+        /// 返回到达 nums[n - 1] 的最小跳跃次数。生成的测试用例可以到达 nums[n - 1]。
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <returns></returns>
+        public static int JumpII(int[] nums)
+        {
+            var sum = nums[0];
+            if (sum >= nums.Length - 1) return 1;
+
+            var count = 0;
+
+            var index = 0;
+            for (int i = nums.Length - 2; i >= 0; i--)
+            {
+                var val = nums[i];
+                if (val >= nums.Length - i - 1)
+                {
+                    count = 1;
+                }
+
+
+                if (val > 0)
+                {
+                    if (val > index)
+                    {
+                        index = 0;
+                        continue;
+                    }
+                }
+                index++;
+            }
+            return 0;
+        }
+        #endregion
+
+        #region 55. 跳跃游戏
+        /// <summary>
+        /// 给你一个非负整数数组nums，你最初位于数组的第一个下标。数组中的每个元素代表你在该位置可以跳跃的最大长度。
+        /// 判断你是否能够到达最后一个下标，如果可以，返回 true ；否则，返回 false 。
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <returns></returns>
+        public static bool CanJump(int[] nums)
+        {
+            var sum = nums[0];
+            if (sum == 0 && nums.Length > 1) return false;
+            if (sum >= nums.Length - 1) return true;
+
+            var index = 0;
+            for (int i = nums.Length - 2; i >= 0; i--)
+            {
+                var val = nums[i];
+                if(val > 0)
+                {
+                    if (val > index)
+                    {
+                        index = 0;
+                        continue;
+                    }
+                }
+                index++;
+            }
+            return nums[0] >= index;
+        }
+
+        #endregion
+
     }
 }
---
+++ SoftLiu_DataStructCSharp/LeetCode/Time20231203.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoftLiu_DataStructCSharp.LeetCode
+{
+    public static class Time20231203
+    {
+        public static void Run()
+        {
+            var carPoints = new int[] { 1, 2, 3, 4, 5, 6, 1 };
+            for (int i = 0; i < carPoints.Length; i++)
+            {
+                Console.Write($"{carPoints[i]},");
+            }
+            Console.WriteLine("");
+            var val = MaxScoreII(carPoints, 3);
+            Console.WriteLine($"{val}, True:{12}");
+        }
+
+        public static int MaxScoreII(int[] cardPoints, int k)
+        {
+            var left = 0;
+            var sumMax = 0;
+            for (int i = 0; i < cardPoints.Length; i++)
+            {
+                sumMax += cardPoints[i];
+            }
+            var minLen = cardPoints.Length - k;
+            var sumMin = 0;            
+
+            for (int i = 0; i < minLen; i++)
+            {
+                sumMin += cardPoints[i];
+            }
+            var sum = sumMin;
+            for (; left + minLen < cardPoints.Length; left++)
+            {
+                sum += cardPoints[left + minLen];
+                sum -= cardPoints[left];
+                sumMin = Math.Min(sum, sumMin);
+            }
+            return sumMax - sumMin;
+        }
+
+
+        public static int MaxScore(int[] cardPoints, int k)
+        {
+            if (cardPoints == null) return 0;
+            if (k > cardPoints.Length) return 0;
+
+            var left = 0;
+            var len = cardPoints.Length;
+            var right = len - 1;
+
+            if (k == 1)
+            {
+                return Math.Max(cardPoints[left], cardPoints[right]);
+            }
+
+            var sumMax = 0;
+            var selectVal = 0;
+            for (int i = 0; i < k; i++)
+            {
+                var leftVal = cardPoints[left];
+                var rightVal = cardPoints[right];
+
+                var leftEnd = left - 1 + k - i;
+                var rightEnd = right - k + i + 1;
+                var leftEndVal = cardPoints[leftEnd];
+                var rightEndVal = cardPoints[rightEnd];
+
+                if (rightVal > leftVal)
+                {
+                    if (rightVal > leftEndVal || (rightVal + rightEndVal > leftEndVal + leftVal))
+                    {
+                        right--;
+                        selectVal = rightVal;
+                    }
+                    else
+                    {
+                        left++;
+                        selectVal = leftVal;
+                    }
+                }
+                else if (leftVal > rightVal)
+                {
+                    // 如果选左边 假设右边一直选的值
+                    if (leftVal > rightEndVal || (leftVal + leftEndVal) > (rightEndVal + rightVal))
+                    {
+                        left++;
+                        selectVal = leftVal;
+                    }
+                    else
+                    {
+                        right--;
+                        selectVal = rightVal;
+                    }
+                }
+                else
+                {
+                    if (leftVal + leftEndVal > rightVal + rightEndVal)
+                    {
+                        left++;
+                        selectVal = leftVal;
+                    }
+                    else
+                    {
+                        right--;
+                        selectVal = rightVal;
+                    }
+                }
+                sumMax += selectVal;
+                Console.WriteLine($"Selct:{selectVal},Sum:{sumMax}, Index:{i}, Left:{left},LeftEnd:{leftEnd}, Right:{right},RightEnd:{rightEnd}");
+            }
+
+            return sumMax;
+        }
+
+    }
+}
