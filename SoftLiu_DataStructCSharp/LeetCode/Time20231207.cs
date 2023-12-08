0df729eac33c539054bfe41f98d46ebfcb8a3289
SunHailang <shl971277663@outlook.com>
2023-12-08 16:21:00 +0800

    add 20231208

--- SoftLiu_DataStructCSharp/LeetCode/Time20231207.cs
+++ SoftLiu_DataStructCSharp/LeetCode/Time20231207.cs
@@ -6,7 +6,54 @@ using System.Threading.Tasks;
 
 namespace SoftLiu_DataStructCSharp.LeetCode
 {
-    class Time20231207
+    /// <summary>
+    /// 1466. 重新规划路线
+    /// </summary>
+    public static class Time20231207
     {
+        public static void Run()
+        {
+
+        }
+        /// <summary>
+        /// n 座城市，从 0 到 n-1 编号，其间共有 n-1 条路线。
+        /// 因此，要想在两座不同城市之间旅行只有唯一一条路线可供选择（路线网形成一颗树）。
+        /// 去年，交通运输部决定重新规划路线，以改变交通拥堵的状况。
+        /// 路线用 connections 表示，其中 connections[i] = [a, b] 表示从城市 a 到 b 的一条有向路线。
+        /// 今年，城市 0 将会举办一场大型比赛，很多游客都想前往城市 0 。
+        /// 请你帮助重新规划路线方向，使每个城市都可以访问城市 0 。返回需要变更方向的最小路线数。
+        /// 题目数据 保证 每个城市在重新规划路线方向后都能到达城市 0 。
+        /// </summary>
+        /// <param name="n"></param>
+        /// <param name="connections"></param>
+        /// <returns></returns>
+        public static int MinReorder(int n, int[][] connections)
+        {
+            IList<Tuple<int, int>>[] e = new IList<Tuple<int, int>>[n];
+            for (int i = 0; i < n; i++)
+            {
+                e[i] = new List<Tuple<int, int>>();
+            }
+            foreach (int[] edge in connections)
+            {
+                e[edge[0]].Add(new Tuple<int, int>(edge[1], 1));
+                e[edge[1]].Add(new Tuple<int, int>(edge[0], 0));
+            }
+            return DFS(0, -1, e);
+        }
+
+        public static int DFS(int x, int parent, IList<Tuple<int, int>>[] e)
+        {
+            int res = 0;
+            foreach (Tuple<int, int> edge in e[x])
+            {
+                if (edge.Item1 == parent)
+                {
+                    continue;
+                }
+                res += edge.Item2 + DFS(edge.Item1, x, e);
+            }
+            return res;
+        }
     }
 }
---
+++ SoftLiu_DataStructCSharp/LeetCode/Time20231208.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoftLiu_DataStructCSharp.LeetCode
+{
+    /// <summary>
+    /// 2008. 出租车的最大盈利
+    /// </summary>
+    public static class Time20231208
+    {
+        public static void Run()
+        {
+
+        }
+
+        /// <summary>
+        /// 你驾驶出租车行驶在一条有 n 个地点的路上。这 n 个地点从近到远编号为 1 到 n ，你想要从 1 开到 n ，通过接乘客订单盈利。
+        /// 你只能沿着编号递增的方向前进，不能改变方向。
+        /// 乘客信息用一个下标从 0 开始的二维数组 rides 表示，其中 rides[i] = [starti, endi, tipi] 表示第 i 位乘客需要从地点 starti 前往 endi ，愿意支付 tipi 元的小费。
+        /// 每一位 你选择接单的乘客 i ，你可以 盈利 endi - starti + tipi 元。你同时 最多 只能接一个订单。
+        /// 给你 n 和 rides ，请你返回在最优接单方案下，你能盈利 最多 多少元。
+        /// 注意：你可以在一个地点放下一位乘客，并在同一个地点接上另一位乘客。
+        /// </summary>
+        /// <param name="n"></param>
+        /// <param name="rides"></param>
+        /// <returns></returns>
+        public static long MaxTaxiEarnings(int n, int[][] rides)
+        {
+            Array.Sort(rides, (a, b) => a[1] - b[1]);
+            int m = rides.Length;
+            long[] dp = new long[m + 1];
+            for (int i = 0; i < m; i++)
+            {
+                int j = BinarySearch(rides, i, rides[i][0]);
+                dp[i + 1] = Math.Max(dp[i], dp[j] + rides[i][1] - rides[i][0] + rides[i][2]);
+            }
+            return dp[m];
+        }
+
+        private static int BinarySearch(int[][] rides, int high, int target)
+        {
+            int low = 0;
+            while (low < high)
+            {
+                int mid = low + (high - low) / 2;
+                if (rides[mid][1] > target)
+                {
+                    high = mid;
+                }
+                else
+                {
+                    low = mid + 1;
+                }
+            }
+            return low;
+        }
+    }
+}
